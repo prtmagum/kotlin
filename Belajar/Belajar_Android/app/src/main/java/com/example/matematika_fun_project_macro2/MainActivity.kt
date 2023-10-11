1a195bc1c1ee0df22f19419b192b8c73a0af5711
Agum Pratama <pratamaagum15@gmail.com>
2023-10-11 14:23:49 +0700

    belajar

---
+++ Belajar/Belajar_Android/app/src/main/java/com/example/matematika_fun_project_macro2/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.matematika_fun_project_macro2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SearchView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
